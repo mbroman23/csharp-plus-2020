8a2a26197f88c40163da67a6a2f08cdf003f4ccf
DESKTOP-I08CG0H\Admin <mbroman23@gmail.com>
2020-08-28 13:44:37 +0200

    Early Commit, not done.

---
+++ Session03Exercise02/Session03Exercise02/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+
+namespace Session03Exercise01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ange ett antal siffror, separerat med kommatecken.");
+            var input = Console.ReadLine();
+            var inputArray = input.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            double?[] numberArray = new double?[inputArray.Length];
+
+            //for (int i = 0; i < inputArray.Length; i++)
+            //{
+            //    try
+            //    {
+            //        numberArray[i] = Convert.ToDouble(inputArray[i]);
+
+            //    }
+
+            //    catch (Exception)
+            //    {
+            //        numberArray[i] = 0;
+            //    }
+
+            //    finally
+            //    {
+            //        Console.WriteLine("Finally skrivs ALLTID!");
+            //    }
+            //}
+
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                NumberStyles numberStyle = NumberStyles.Integer | NumberStyles.Float;
+
+                bool parsed = double.TryParse(inputArray[i], numberStyle, CultureInfo.InvariantCulture, out double parsedValue);
+
+                if (parsed)
+                {
+                    numberArray[i] = parsedValue;
+                }
+                else
+                {
+                    numberArray[i] = null;
+                }
+            }
+
+            foreach (var number in numberArray)
+            {
+                Console.WriteLine("Värdet är " + number.ToString());
+            }
+        }
+    }
+}
