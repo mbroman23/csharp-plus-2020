a3ef60006eeeebee73b5c9372955cdd592adff2d
DESKTOP-I08CG0H\Admin <mbroman23@gmail.com>
2020-08-28 09:26:05 +0200

    Session03Commit

---
+++ Session02/Session02Exercise01/Session02Exercise02/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Session02Exercise02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Do you want icecream?");
+
+            var key = Console.ReadKey();
+            if (key.KeyChar == 'n')
+            {
+                return;
+            }
+
+            else
+            {
+                Console.WriteLine("Enter your flavour: ");
+                var flavour = Console.ReadLine();
+                Console.WriteLine("Here is your " + flavour + "-icecream!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
